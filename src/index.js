aeccbd5a0e50937eb0c70e02f6d632e0e9073268
Daniel Costa <danieljscosta@gmail.com>
2024-09-16 19:07:16 -0300

    rota get projects

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/projects', function(request, response){
+    return response.json([
+        'Projeto 1',
+        'Projeto 2'
+    ])
+})
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000!')
+})
