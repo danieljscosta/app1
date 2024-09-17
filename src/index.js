dcbff693129972c2b445c04b6ffb86e5cec5927f
Daniel Costa <danieljscosta@gmail.com>
2024-09-17 19:08:45 -0300

    aula completa aplicacao completa

--- src/index.js
+++ src/index.js
@@ -1,39 +1,75 @@
 const express = require('express')
+const {v4: uuidv4} = require('uuid')
 
 const app = express()
+app.use(express.json())
+
+//Irá simular nosso BD
+const projects = []
+
+//Criação do middleware
+function logRouters(request, response, next){
+    const {method, url} = request
+    const route = `[${method.toUpperCase()}] ${url}`
+    console.log(route)
+    return next()
+}
+
+//app.use(logRouters)
 
 app.get('/projects', function(request, response){
-    const {title, owner, page} = request.query
-    console.log(title, owner, page)
-    return response.json([
-        'Projeto 1',
-        'Projeto 2'
-    ])
+    return response.json(projects)
 })
 
-app.post('/projects', function(request, response){
-    return response.json([
-        'Projeto 1',
-        'Projeto 2',
-        'Projeto 3'
-    ])
+app.post('/projects', logRouters ,function(request, response){
+    const {name , owner} = request.body
+    const project = {
+        id: uuidv4(),
+        name,
+        owner
+    }
+    //coloco no array o objeto projeto que acabou de ser criado
+    projects.push(project)
+    return response.status(201).json(project)
 })
 
-app.put('/projects/:id/:name', (req, res)=>{
-    const {id, name} = req.params
+app.put('/projects/:id', (req, res)=>{
+    const {id} = req.params
+    const {name, owner} = req.body
+
+    const projectIndex = projects.findIndex(p => p.id === id)
+
+    if(projectIndex < 0){
+        return res.status(404).json({ erro: 'Project not found' })
+    }
+    //bad request (ficou faltando na request a pesoa passar o nome e o proprietario)
+    if(!name || !owner){
+        return res.status(400).json({ error: 'Name and owner are required'})
+    }
+
+    const project = {
+        id,
+        name,
+        owner
+    }
+    projects[projectIndex] = project
+
     console.log(id, name)
-    return res.json([
-        'Projeto 4',
-        'Projeto 2',
-        'Projeto 3'
-    ])
+
+    return res.json(project)
 })
 
 app.delete('/projects/:id', (req, res) => {
-    return res.json([
-        'Projeto 2',
-        'Projeto 3'
-    ]) 
+    const {id} = req.params
+    const projectIndex = projects.findIndex(p => p.id === id)
+
+    if(projectIndex < 0){
+        return res.status(404).json({ error: 'Project not found'})
+    }
+
+    projects.slice(projectIndex, 1)
+
+    return res.status(204).send()
 })
 
 app.listen(3000, () => {
